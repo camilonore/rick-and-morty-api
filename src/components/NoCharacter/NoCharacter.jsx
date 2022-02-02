3da9acd93c8b7217209ab3d5a0bd8f6960e2b98b
Camilo292 <caminore22@gmail.com>
2022-02-02 16:03:54 -0500

    Add no character section

---
+++ src/components/NoCharacter/NoCharacter.jsx
@@ -0,0 +1,10 @@
+import './NoCharacter.css'
+
+export default function NoCharacter () {
+  return(
+    <div className='no-character-container'>
+      <h3>The character doesn't exists</h3>
+      <img src="/noCharacter.png" alt="Rick and Morty" />
+    </div>
+  )
+}
