b2c704c79bd8874636afc8f9e68b0beb08f639e6
Camilo292 <caminore22@gmail.com>
2022-02-04 12:37:46 -0500

    Added Responsive bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className='navbar'>
-      <a href='#characters'>Characters</a>
+      <input type="checkbox" id='burger-checkbox'/>
+      <label htmlFor="burger-checkbox" className='burger-label'></label>
       <Link to='/' className='logo'>
         <img src='/rickLogo.png' alt='Rick and Morty' />
       </Link>
-      <a href='#footer'>Footer</a>
+      <a href='#characters' id='characters-link'>Characters</a>
+      <a href='#footer' id='footer-link'>Footer</a>
     </header>
   );
 }
